48e75464798fc4f5f9713054f03444b80b8d0d85
gnehs <jayuiop@gmail.com>
2022-04-21 10:14:33 +0800

    added theme

--- ntut-istudy-pdf-downloader.user.js
+++ ntut-istudy-pdf-downloader.user.js
@@ -1,14 +1,18 @@
 // ==UserScript==
 // @name         北科 i 學園 PDF 下載工具
 // @namespace    https://gnehs.net/
-// @version      0.2
+// @version      0.2.1
 // @description  協助尼下載北科 i 學園 PDF 的好朋友
 // @author       gnehs
 // @match        https://istudy.ntut.edu.tw/learn/path/viewPDF.php*
 // @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=edu.tw
 // @grant        none
-// ==/UserScript==
+// ==/UserScript== 
+let swaltheme = document.createElement('link');
+swaltheme.rel = 'stylesheet';
+swaltheme.href = `https://cdn.jsdelivr.net/npm/@sweetalert2/theme-borderless@5.0.7/borderless.min.css`
+document.getElementsByTagName('head')[0].appendChild(swaltheme);
 function addGlobalStyle(css) {
   var head, style;
   head = document.getElementsByTagName('head')[0];
