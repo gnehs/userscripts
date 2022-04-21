05c86733482c7f5df7c88b1ad299748f17430c4d
gnehs <jayuiop@gmail.com>
2022-04-21 10:02:33 +0800

    updated ntut-istudy-pdf-downloader.user.js

--- ntut-istudy-pdf-downloader.user.js
+++ ntut-istudy-pdf-downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         北科 i 學園 PDF 下載工具
 // @namespace    https://gnehs.net/
-// @version      0.1
+// @version      0.2
 // @description  協助尼下載北科 i 學園 PDF 的好朋友
 // @author       gnehs
 // @match        https://istudy.ntut.edu.tw/learn/path/viewPDF.php*
@@ -21,22 +21,39 @@ function addGlobalStyle(css) {
 
 addGlobalStyle('.swal2-container{z-index: 100000000000; !important}');
 window.addEventListener("load", async function (event) {
-  let url_string = "https://istudy.ntut.edu.tw/learn/path/" + DEFAULT_URL;
+  let url_string = "https://istudy.ntut.edu.tw/learn/path/" + window.DEFAULT_URL;
   let url = new URL(url_string);
-  let u = await fetch(url_string, {
-    method: 'GET'
-  })
-    .then(response => response.blob())
-    .then(blob => {
-      return window.URL.createObjectURL(blob);
-    });
   Swal.fire({
     toast: true,
     position: 'top-end',
     showConfirmButton: true,
-    title: `下載`,
-    confirmButtonText: "關閉",
-    html:
-      `檔案：<a href="${u}" download="${url.searchParams.get("id")}">下載</a><br/>`
+    showCancelButton: true,
+    title: `北科 i 學園 PDF 下載工具`,
+    confirmButtonText: `下載 ${url.searchParams.get("id")}`,
+    cancelButtonText: "關閉",
+    html: ``
+  }).then(async (result) => {
+    if (result.value) {
+      let loader = Swal.fire({
+        toast: true,
+        position: 'top-end',
+        title: `正在下載「${url.searchParams.get("id")}」`,
+        html: `下載中...`,
+        showConfirmButton: false,
+        didOpen: () => {
+          Swal.showLoading()
+        }
+      })
+      let u = await fetch(url_string, { method: 'GET' })
+        .then(response => response.blob())
+        .then(blob => {
+          return window.URL.createObjectURL(blob);
+        });
+      loader.close()
+      let a = document.createElement('a');
+      a.href = u;
+      a.download = url.searchParams.get("id");
+      a.click();
+    }
   })
 });
