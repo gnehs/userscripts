217295e1c447964b6db8b34cbbe7b86171163ed5
gnehs <jayuiop@gmail.com>
2022-05-20 14:10:43 +0800

    updated skillsforall

--- skillsforall.user.js
+++ skillsforall.user.js
@@ -1,19 +1,13 @@
 // ==UserScript==
-// @name         Skill for all fix
+// @name         fix skills for all base64 decode
 // @namespace    http://gnehs.net/
-// @version      0.2
+// @version      0.3
 // @description  修正 Base64 解析錯誤
 // @author       gnehs
 // @match        https://skillsforall.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=skillsforall.com
 // @grant        none
 // @require      https://cdn.jsdelivr.net/gh/ethereumjs/browser-builds/dist/ethereumjs-tx/ethereumjs-tx-1.3.3.min.js
-// ==/UserScript== 
+// ==/UserScript==
 window.atob = val => ethereumjs.Buffer.Buffer.from(val, 'base64').toString()
-let originalJSONParse = JSON.parse
-
-JSON.parse = val => {
-  console.log('[JSON.parse]', val)
-  return val ? originalJSONParse(val) : {}
-}
 window.escape = x => x
