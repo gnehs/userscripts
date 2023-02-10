e700d5a179670b62fdc2466dc0f3b3d326c70289
gnehs <jayuiop@gmail.com>
2023-02-10 17:14:59 +0800

    shorter v1 prod link

--- pchome-link-copy.user.js
+++ pchome-link-copy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PChome link Fix
 // @namespace    https://gnehs.net/
-// @version      0.2
+// @version      0.3
 // @description  複製帶有預覽的 PChome 連結
 // @author       gnehs
 // @match        https://24h.pchome.com.tw/prod/*
@@ -42,7 +42,9 @@ let buttonStyle = `
 `
 GM_addStyle(buttonStyle)
 
-let link = location.href.replace('https://24h.pchome.com.tw', 'https://p.pancake.tw');
+let link = location.href
+  .replace('https://24h.pchome.com.tw', 'https://p.pancake.tw')
+  .replace('/prod/v1/', '/prod/');
 let button = document.createElement("button");
 let copySvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M20 2H10c-1.103 0-2 .897-2 2v4H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2v-4h4c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zM4 20V10h10l.002 10H4zm16-6h-4v-4c0-1.103-.897-2-2-2h-4V4h10v10z"></path></svg>`
 let checkSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg>`
