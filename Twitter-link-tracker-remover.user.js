dc74b1210bbda50aa81edc11abde1c98a3ebb7e5
gnehs <gnehs@gnehs.net>
2024-06-30 23:29:07 +0800

    updated Twitter-link-tracker-remover

--- Twitter-link-tracker-remover.user.js
+++ Twitter-link-tracker-remover.user.js
@@ -2,23 +2,23 @@
 // @name               Twitter link tracker remover
 // @name:zh-TW         Twitter 追蹤連結移除器
 // @namespace          https://gnehs.net/
-// @version            0.3.1
+// @version            0.4.0
 // @description        Remove Twitter's link tracker, and replace it with vxtwitter link.
 // @description:zh-TW  移除 Twitter 的連結追蹤器，並將連結改為 vxtwitter 連結來修復預覽圖片。
 // @author             gnehs
 // @match              https://x.com/*
-// @icon               https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @icon               https://www.google.com/s2/favicons?sz=64&domain=x.com
 // @grant              none
 // ==/UserScript==
 if (!localStorage["alert.msg"]) {
   let lang = navigator.language || navigator.userLanguage;
   if (lang === "zh-TW") {
     alert(
-      "感謝安裝 Link tracker remover (Twitter)，請點擊「允許」按鈕允許該腳本讀取你的剪貼簿。"
+      "感謝安裝 Twitter 追蹤連結移除器，請點擊「允許」按鈕允許該腳本讀取你的剪貼簿。"
     );
   } else {
     alert(
-      'Thanks to the installation of Link tracker remover (Twitter), you need to allow the script to read your clipboard by clicking the "Allow" button.'
+      'Thanks to the installation of Twitter link tracker remover, you need to allow the script to read your clipboard by clicking the "Allow" button.'
     );
   }
   navigator.clipboard.readText();
@@ -31,7 +31,7 @@ document.addEventListener("copy", async (event) => {
       let link = new URL(text);
       link.search = "";
       link.hash = "";
-      link.host = "vxtwitter.com";
+      link.host = "fixvx.com";
       await navigator.clipboard.writeText(link.href);
     } catch (e) {
       console.log(e);
